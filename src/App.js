285306557513580d99781cde34a6ea2638201197
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-25 14:05:04 -0500

    pushh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+/*import ApolloClient from 'apollo-boost';
+/*
+...
+*/
+// components
+import TaskList from './components/TaskList';
+import AddTask from './components/AddTask';
+import AddProject from './components/AddProject';
+// apollo client setup
+/*
+...
+*/
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <
+    div id = "main" >
+    <
+    div id = "bg" > < /div> <
+    h1 > Holberton school tasks < /h1> <
+    TaskList / >
+    <
+    AddProject / >
+    <
+    AddTask / >
+    <
+    /div>
   );
 }
 
